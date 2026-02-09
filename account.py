768d8c6ace9e5d72b18b6c0e928b5cff7a3a306a
Akshat Pandey <136919464+akshat37@users.noreply.github.com>
2026-02-09 19:47:01 +0530

    Add files via upload

---
+++ account.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate('pondering-tutorial-a608e-1c2dd27d1cbb.json')
+
+# firebase_admin.initialize_app(cred)
+
+def app():
+
+    st.title('Welcome to :violet[Pondering!] :sun_with_face:')
+    # choice = st.selectbox('Login/Signup', ['Login', 'Signup'])
+
+    if 'username' not in st.session_state:
+        st.session_state.username = ''
+    if 'useremail' not in st.session_state:
+        st.session_state.useremail = ''    
+
+    def f():
+        try:
+            user = auth.get_user_by_email(email)
+            st.success('Logged in successfully!')
+            st.markdown('Welcome back, ' + user.uid + '!')
+            st.session_state.username = user.uid
+            st.session_state.useremail = user.email
+            st.session_state.signedout = True
+            st.session_state.signout = True
+
+        except:
+            st.warning('Invalid email or password. Please try again.') 
+
+    def t():
+        st.session_state.signedout = False
+        st.session_state.signout = False
+        st.session_state.username = ''
+                 
+
+    if 'signedout' not in st.session_state:
+        st.session_state.signedout = False 
+    if 'signout' not in st.session_state:
+        st.session_state.signout = False  
+
+
+
+
+
+    if not st.session_state['signedout']:
+        choice = st.selectbox('Login/Signup', ['Login', 'Signup'])
+
+        if choice == 'Login':
+            email = st.text_input('Email Address')
+            password = st.text_input('Password', type='password')
+
+            st.button(':green[Login]', on_click=f)
+
+        else :   
+            email = st.text_input('Email Address')
+            password = st.text_input('Password', type='password')
+            confirm_password = st.text_input('Confirm Password', type='password')
+            username = st.text_input('Enter your unique username')
+
+            if st.button('Create my account'):
+                user = auth.create_user(
+                    email=email, 
+                    password=password,
+                    uid=username
+                )
+                st.success('Account created successfully!')
+                st.markdown('Please Login using your email and password')
+                st.balloons()
+
+    if st.session_state.signout:
+        st.text('Name '+st.session_state.username)
+        st.text('Email id: '+st.session_state.useremail)
+        st.button('Sign out', on_click=t) 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+firebase_admin.initialize_app(cred)
